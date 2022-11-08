49ef5499285315fe86407b2bfead985cb4c7d5d8
preeti-bharti <preeti_bharti@epam.com>
2022-11-08 14:16:38 +0530

    change name for creating PR

--- src/app/features/login/login.component.ts
+++ src/app/features/login/login.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-  User:string="Dave";
+  User:string="David";
   constructor() { 
   }
 
